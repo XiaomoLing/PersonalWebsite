4f9c08e6292fded372a7cc8a8d93c90309f9eae5
Chrisfufu <zuofu@ualberta.ca>
2021-04-05 21:17:39 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -7,31 +7,26 @@ import AppLayout from "./pages/layout";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./pages/about";
 import News from "./pages/news";
-import Publications from "./pages/publications";
-import Teaching from "./pages/teaching";
-import Projects from "./pages/projects";
+// import Publications from "./pages/publications";
+// import Teaching from "./pages/teaching";
+// import Projects from "./pages/projects";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="">
-					<About />
-				</Route>
 				<Route exact path="/about">
 					<About />
 				</Route>
-				<Route exact path="/publications">
-					<Publications />
-				</Route>
-				<Route exact path="/teaching">
+				{/* <Route exact path="/teaching">
 					<Teaching />
-				</Route>
-				<Route exact path="/projects">
-					<Projects />
-				</Route>
+				</Route> */}
 				<Route exact path="/news">
 					<News />
+				</Route>
+				{/* path="" put in the end */}
+				<Route exact path="">
+					<About />
 				</Route>
 				{/* <Route component={About}></Route> */}
 				{/* <Route><AppLayout/></Route> */}
