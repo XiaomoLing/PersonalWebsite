f02216096f2b66efc553e4907c95976985a5aa7b
Chrisfufu <zuofu@ualberta.ca>
2021-04-20 11:55:50 -0600

    trying to fix router

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Projects from "./pages/projects";
 
 function App() {
 	return (
-		<Router>
+		<Router basename={process.env.PUBLIC_URL}>
 			<Switch>
 				<Route exact path="/PersonalWebsite/about">
 					<About />
