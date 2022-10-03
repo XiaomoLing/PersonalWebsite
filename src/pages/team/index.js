4eb19618fb4869ff045b121339f01dfbccf48899
Zuofu Li <zuofu.li@vogappdevelopers.com>
2022-10-02 23:32:07 -0600

    update team page

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,172 +1,177 @@
-import React from 'react';
+import React from "react";
 
-import 'antd/dist/antd.css';
-import './index.css';
+import "./index.css";
 
-import { Typography } from 'antd';
-import AppLayout from '../layout';
-import { Divider } from 'antd';
-import { Row, Col } from 'antd';
-const { Paragraph, Text, Title, Link} = Typography;
-
-
+import { Typography } from "antd";
+import AppLayout from "../layout";
+import { Divider } from "antd";
+import { Row, Col } from "antd";
+const { Paragraph, Text, Title, Link } = Typography;
+// import KimiGigi from "./"
 
 class Team extends React.Component {
-    state = {
-        current: 'mail',
-      };
+	state = {
+		current: "mail",
+	};
 
-      render() {
-        const { current } = this.state;
-        return (
-    
-          <AppLayout>
-            <div style={{
-              display: 'inline'
-            }}>
-    
-              <Divider>
-                <Title level={4}>WE ARE HIRING!</Title>        
-              </Divider>
-    
-              <Paragraph >
-                <Text strong>
-                Thank you for your interest in joining our research group!
-                </Text>
-              </Paragraph>
-    
-              <Paragraph >
-                We are always on the lookout for exceptionally bright and eager students to join our 
-                team. I supervise undergraduate, MASc and PhD students in Construction Management 
-                at UW. I am looking for students who are:
-                <ul>
-                  <li>
-                    passionate about challenging the status quo; 
-                  </li>
-                  <li>
-                    problem solver; 
-                  </li>
-                  <li>
-                    good communicators; and 
-                  </li>
-                  <li>
-                    independent and critical thinkers.
-                  </li>
-                </ul>
-              </Paragraph>
-    
-              <Paragraph >
-                Graduate Research Assistant (GRA) positions are limited and competitive. 
-                Students who pursue and receive funding through outside scholarships or 
-                fellowships greatly increase their likelihood of admission. I keep the 
-                team moderately sized so that I can work closely with every student in the group.
-              </Paragraph>
+	render() {
+		const { current } = this.state;
+		return (
+			<AppLayout>
+				<div
+					style={{
+						display: "inline",
+					}}
+				>
+					<Divider>
+						<Title level={4}>WE ARE HIRING!</Title>
+					</Divider>
 
-              <Paragraph >
-               If you would like to join our team, please do the following:
-                <ol>
-                    <li>
-                      Review our recent work on this website to ensure it aligns with your research interests.
-                    </li>
-                    <li>
-                      Review information about the programs (
-                      <Link href="https://cm.be.uw.edu/students/grad-prof-programs/graduate-admission/" target="_blank">
-                      MSc </Link>and <Link href="https://be.uw.edu/academics/phd/admissions-overview/" target="_blank">
-                      BE Ph.D</Link>) and be sure you meet all the criteria.
-                    </li>
-                    <li>
-                      Contact Dr. Lingzi Wu by email with:
-                        <ol>
-                            <li>
-                             A subject line of “Prospective Student_Undergrad/MSc/PhD_Firstname Lastname”
-                            </li>
-                            <li>Your latest CV,</li>
-                            <li>Your transcript, and</li>
-                            <li>
-                             One example of past work (e.g., published/unpublished paper, project, report etc.) Please clearly identify your contribution if there are multiple authors. 
-                            </li>
-                        </ol>
-                      </li>  
-                </ol>
-              </Paragraph>
+					<Paragraph>
+						<Text strong>
+							Thank you for your interest in joining our research group!
+						</Text>
+					</Paragraph>
 
-              <Paragraph>
-                Please note that due to the volume of emails, I am generally unable to acknowledge receipt.
-              </Paragraph>
-    
-              <Divider>
-                <Title level={4}>TEAM MEMBER</Title>        
-              </Divider>
-    
-              <Row justify="space-around">
-            <Col span={10} style={{
-              marginLeft: "5%"
-            }}>
+					<Paragraph>
+						We are always on the lookout for exceptionally bright and eager students
+						to join our team. I supervise undergraduate, MASc and PhD students in
+						Construction Management at UW. I am looking for students who are:
+						<ul>
+							<li>passionate about challenging the status quo;</li>
+							<li>problem solver;</li>
+							<li>good communicators; and</li>
+							<li>independent and critical thinkers.</li>
+						</ul>
+					</Paragraph>
 
-              <Paragraph >         
-                <Title level={5}>Elnaz Jafari |<em> MSc Student</em><br></br>
-                co-supervised</Title>  
-               </Paragraph> 
+					<Paragraph>
+						Graduate Research Assistant (GRA) positions are limited and competitive.
+						Students who pursue and receive funding through outside scholarships or
+						fellowships greatly increase their likelihood of admission. I keep the
+						team moderately sized so that I can work closely with every student in the
+						group.
+					</Paragraph>
 
-              <Paragraph >  
-                <img
-                  width={350}
-                  src={`${process.env.PUBLIC_URL}/images/KimiGigi Circle.png`} 
-                />
-                <Title level={5}>Kimi and Gigi |<em> Happiness Coordinators </em></Title>        
-              </Paragraph> 
+					<Paragraph>
+						If you would like to join our team, please do the following:
+						<ol>
+							<li>
+								Review our recent work on this website to ensure it aligns with your
+								research interests.
+							</li>
+							<li>
+								Review information about the programs (
+								<Link
+									href="https://cm.be.uw.edu/students/grad-prof-programs/graduate-admission/"
+									target="_blank"
+								>
+									MSc{" "}
+								</Link>
+								and{" "}
+								<Link
+									href="https://be.uw.edu/academics/phd/admissions-overview/"
+									target="_blank"
+								>
+									BE Ph.D
+								</Link>
+								) and be sure you meet all the criteria.
+							</li>
+							<li>
+								Contact Dr. Lingzi Wu by email with:
+								<ol>
+									<li>
+										A subject line of “Prospective Student_Undergrad/MSc/PhD_Firstname
+										Lastname”
+									</li>
+									<li>Your latest CV,</li>
+									<li>Your transcript, and</li>
+									<li>
+										One example of past work (e.g., published/unpublished paper, project,
+										report etc.) Please clearly identify your contribution if there are
+										multiple authors.
+									</li>
+								</ol>
+							</li>
+						</ol>
+					</Paragraph>
 
-            </Col>
+					<Paragraph>
+						Please note that due to the volume of emails, I am generally unable to
+						acknowledge receipt.
+					</Paragraph>
 
-            <Col span = {10} style={{
-              marginRight: "5%"
-            }}>
-              <Paragraph >             
-              {/* <img
-                width={350}
-                src={`${process.env.PUBLIC_URL}/images/KimiGigi Circle.png`} 
-              />
-              </Paragraph>        
-              
-              <Paragraph >                 */}
-                <Title level={5}>Zuofu (Chris) Li |<em> Website Developer </em></Title>
-              </Paragraph>
-              
+					<Divider>
+						<Title level={4}>TEAM MEMBER</Title>
+					</Divider>
 
-            </Col>
-          </Row>
+					<Row justify="space-around">
+						<Col span={8} style={{ justifyContent: "center", display: "grid" }}>
+							<Paragraph style={{ height: "50px" }}>
+								<Title level={5} style={{ textAlign: "center" }}>
+									Elnaz Jafari |<em> MSc Student</em>
+									<br></br>
+									co-supervised
+								</Title>
+							</Paragraph>
 
+							<div className="background"></div>
+							<Paragraph>
+								<Title level={5} style={{ textAlign: "center" }}>
+									Kimi and Gigi |<em> Happiness Coordinators </em>
+								</Title>
+							</Paragraph>
+						</Col>
 
-              <Divider>
-                <Title level={4}>FORMER STUDENTS</Title>        
-              </Divider>
+						<Col span={8} style={{ justifyContent: "center", display: "grid" }}>
+							<Paragraph style={{ height: "50px" }}>
+								<Title level={5} style={{ textAlign: "center" }}>
+									Zuofu (Chris) Li |<em> Website Developer </em>
+								</Title>
+							</Paragraph>
+							<div className="zuofubg"></div>
+							<div style={{ height:"32px" }}></div>
+						</Col>
+					</Row>
 
-              <Paragraph >         
-                <Title level={5}>Xiang Mao  |<em> MEng Student</em><br></br>
-                co-supervised</Title>  
-              </Paragraph> 
+					<Divider>
+						<Title level={4}>FORMER STUDENTS</Title>
+					</Divider>
 
-              <Paragraph >         
-                <Title level={5}>Kunkun Li |<em> MEng Student</em><br></br>
-                co-supervised</Title>  
-              </Paragraph> 
+					<Paragraph>
+						<Title level={5}>
+							Xiang Mao |<em> MEng Student</em>
+							<br></br>
+							co-supervised
+						</Title>
+					</Paragraph>
 
-              <Paragraph >         
-                <Title level={5}>Zuofu Li |<em> Undergraduate Coop Student</em><br></br>
-                co-supervised</Title>  
-              </Paragraph> 
+					<Paragraph>
+						<Title level={5}>
+							Kunkun Li |<em> MEng Student</em>
+							<br></br>
+							co-supervised
+						</Title>
+					</Paragraph>
 
-              <Paragraph >         
-                <Title level={5}>Baoli Feng |<em> Undergraduate Research Student</em><br></br>
-                co-supervised</Title>  
-              </Paragraph> 
+					<Paragraph>
+						<Title level={5}>
+							Zuofu Li |<em> Undergraduate Coop Student</em>
+							<br></br>
+							co-supervised
+						</Title>
+					</Paragraph>
 
-            </div>
-            
-          </AppLayout>
-    
-          
-        );
-      }
+					<Paragraph>
+						<Title level={5}>
+							Baoli Feng |<em> Undergraduate Research Student</em>
+							<br></br>
+							co-supervised
+						</Title>
+					</Paragraph>
+				</div>
+			</AppLayout>
+		);
+	}
 }
 export default Team;
